4695d59cbe09783dd8ec490d5f48f6377ea26189
Ivan <toftul.ivan@gmail.com>
2017-07-20 18:03:07 +0300

    second big impackt
    1. mu calculations
    2. main structure of G calculations

--- main.py
+++ main.py
@@ -13,8 +13,6 @@ import progressbar
 import scipy.special as sp
 # import my libs
 import const
-import prm
-import extra
 
 
 
@@ -29,6 +27,7 @@ gamma = 1.  # phenomenological parameter
 epsilon_fiber = 1.
 epsilon_particle = 1.
 epsilon_m = 1.
+alpha0 = 1.
 
 hight = 1. 
 
@@ -38,12 +37,12 @@ tmax = 1.
 dt = 1.
 dr = 1.
 
+# r = {rho=x, y, z}
 dip_r = np.zeros([2, 3])
-dip_r[0] = np.array([0., 0., hight])
-dip_r[1] = np.array([0., 5., hight])
+dip_r[0] = np.array([hight, 0., 0.])
+dip_r[1] = np.array([hight, 0., 5.])
 dip_v = np.zeros([2, 3])
 dip_mu = np.zeros([2, 3], dtype=complex)
-dip_mass = np.ones(2) * m_charastic
 
 
 
@@ -60,21 +59,20 @@ time_space = np.linspace(0, tmax, step_num + 1)
 z_for_force_calc = np.linspace(0, 30)
 
 
-def G0(Rmod, wl):
-    result = np.zeros([3, 3])
+def G0zz(r1, r2, wl):
+    Gzz = 0.
+    Rmod = np.linalg.norm(r1 - r2)
     if Rmod != 0:
-        kR = k0(wl) * Rmod
+        kR = k1(wl) * Rmod
         EXPikR4piR = np.exp(1j * kR) / (4. * np.pi * Rmod)
-        CONST1 = (1 + (1j * kR - 1) / kR**2)
-        CONST2 = (3 - 3j * kR - kR**2) / (kR**2 * Rmod**2)
-        # return Green function
-        result = EXPikR4piR * (CONST1 * np.identity(3) +
-                               CONST2 * np.outer(R, R))
+        A = 1 + (1j * kR - 1) / kR**2
+        B = (3 - 3j * kR - kR**2) / (kR**2 * Rmod**2) \
+                 * (r1[2] - r2[2])**2 / Rmod**2
+        Gzz = EXPikR4piR * (A + B)
     else:
         print("G0 ERROR! Rmod = 0 leads to singularity.")
-
-    return result
-
+        
+    return(Gzz)
 
 
 #
@@ -92,6 +90,7 @@ def G0(Rmod, wl):
 #
 #
 # for normal incidence kz = 0
+# let particles has the same hight -> rho = rho'
 def A_prime(wl):
     return(0)
 def B_prime(wl):
@@ -102,15 +101,17 @@ def F(wl):
     return(0)
 def P11NN(wl):
     return(0)
-def Gszz(rho, rho_prime, dz, wl=wave_length):
+def Gszz(rho, rho_prime, wl=wave_length):
     G = -0.5 * P11NN(wl) * krho1(wl)**2 / (k1(wl)**2) * \
         sp.hankel1(0, krho1 * rho) * sp.hankel1(0, krho1 * rho_prime)
 
-
-def mu_solver():
+# mu = alpha_eff * E0
+def alpha_eff(wl):
+    # Gs(r1, r1) = Gs(r1, r2) = Gs(r2,r1) = Gs(r2, r2)
+    GGG = 2 * Gszz(dip_r[0, 0], dip_r[0, 0]) + G0zz(dip_r[0], dip_r[1], wl)
+    return(alpha0 / (1 - alpha0 * k1(wl)**2 / const.epsilon0 * GGG))
     
-
 
 def force(z, wl=wave_length):
-    
+    return(0)
     

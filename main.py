f8a1c5c5097fc6d224a3682109a45508364bd661
Ivan <toftul.ivan@gmail.com>
2017-07-21 17:13:40 +0300

    Changed G for calculations with numpy arrays

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ n_times_k0 = 1
 
 tmax = 1.
 dt = 1.
-dr = 1.
+dz = 1.
 
 # r = {rho=x, y, z}
 dip_r = np.zeros([2, 3])
@@ -58,19 +58,19 @@ time_space = np.linspace(0, tmax, step_num + 1)
 
 z_for_force_calc = np.linspace(0, 30)
 
+# incident wave vector
+def k1(wl):
+    return(2 * np.pi / wl)
 
-def G0zz(r1, r2, wl):
-    Gzz = 0.
-    Rmod = np.linalg.norm(r1 - r2)
-    if Rmod != 0:
-        kR = k1(wl) * Rmod
-        EXPikR4piR = np.exp(1j * kR) / (4. * np.pi * Rmod)
-        A = 1 + (1j * kR - 1) / kR**2
-        B = (3 - 3j * kR - kR**2) / (kR**2 * Rmod**2) \
-                 * (r1[2] - r2[2])**2 / Rmod**2
-        Gzz = EXPikR4piR * (A + B)
-    else:
-        print("G0 ERROR! Rmod = 0 leads to singularity.")
+def G0zz(x1, y1, z1, x2, y2, z2, wl):
+    Rmod = np.sqrt((x1-x2)**2 + (y1-y2)**2 + (z1-z2)**2)
+    
+    kR = k1(wl) * Rmod
+    EXPikR4piR = np.exp(1j * kR) / (4. * np.pi * Rmod)
+    A = 1 + (1j * kR - 1) / kR**2
+    B = (3 - 3j * kR - kR**2) / (kR**2 * Rmod**2) \
+                * (z1 - z2)**2 / Rmod**2
+    Gzz = EXPikR4piR * (A + B)
         
     return(Gzz)
 
@@ -79,17 +79,34 @@ def G0zz(r1, r2, wl):
 #   1 rho                 
 #   |                  |------> E0
 #   |                  |  
-#   |    /-\           |
+#   |    /-\           |      incident wave
 #   |   | * |          v k1
 #   |    \-/
-#  ##############################-----> z
+#   |
+#  ##############################
 #  ~                            ~
-#  ~            FIBER           ~
+#  ~           FIBER            ~ -----> z
 #  ~                            ~
 #  ##############################
 #
 #
 # let particles has the same hight -> rho = rho'
+
+# waveguide wave vector 
+def k1_wg():
+    return(0)
+def kz_wg():
+    return(0)
+def krho1_wg():
+    return(0)
+def krho2_wg():
+    return(0)
+def A():
+    return(0)
+def B():
+    return(0)
+def C():
+    return(0)
 def A_prime(wl):
     return(0)
 def B_prime(wl):
@@ -98,20 +115,43 @@ def C_prime(wl):
     return(0)
 def F(wl):
     return(0)
-def P11NN(wl):
+def P11NN():
     return(0)
 def Gszz(rho, rho_prime, dz, wl=wave_length):
-    G = -0.5 * P11NN(wl) * krho1(wl)**2 / (k1(wl)**2) \
-        * sp.hankel1(0, krho1 * rho) * sp.hankel1(0, krho1 * rho_prime) \
-        * np.exp(1j * kz * np.abs(dz))
+    G = -0.5 * P11NN() * krho1_wg()**2 / (k1_wg()**2) \
+        * sp.hankel1(0, krho1_wg() * rho) * sp.hankel1(0, krho1_wg() * rho_prime) \
+        * np.exp(1j * kz_wg() * np.abs(dz))
+    return(G)
 
-# mu = alpha_eff * E0
+# mu_12 = alpha_eff * E0
+# ( 1,2 -- only if rho1 = rho2 )
 def alpha_eff(wl):
-    # Gs(r1, r1) = Gs(r1, r2) = Gs(r2,r1) = Gs(r2, r2)
-    GGG = 2 * Gszz(dip_r[0, 0], dip_r[0, 0]) + G0zz(dip_r[0], dip_r[1], wl)
+    # Gs(r1, r2) = Gs(r2,r1)
+    GGG = Gszz(dip_r[0, 0], dip_r[0, 0], 0.) + \
+          Gszz(dip_r[0, 0], dip_r[1, 0], dip_r[0, 2] - dip_r[1, 2]) + \
+          G0zz(dip_r[0], dip_r[1], wl)
     return(alpha0 / (1 - alpha0 * k1(wl)**2 / const.epsilon0 * GGG))
     
 
-def force(z, wl=wave_length):
-    return(0)
+# z may be a numpy array! Should be fast
+# considered that d/dz mu = 0
+def force(rho1, rho2, z, wl=wave_length):
+    al_eff = alpha_eff(wl)
+    Fz = 0.5 * al_eff * al_eff.conjugate() * \
+         E0_charastic * E0_charastic.conjugate() / const.epsilon0
+         
+    z_plus = z + dz
+    z_minus = z - dz
+    
+    G_plus =  Gszz(rho1, rho2, z_plus) + \
+              G0zz(rho1, 0., 0., rho2, 0., z_plus, wl)
+              
+    G_minus = Gszz(rho1, rho2, z_minus) + \
+              G0zz(rho1, 0., 0., rho2, 0., z_minus, wl)
+              
+    RE = k1(wl)**2 * (G_plus - G_minus) * 0.5 / dz
+    
+    return(Fz * RE.real)
+
+
     

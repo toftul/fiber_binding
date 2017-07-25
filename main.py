12d3f460fb636c81cfaa9677d5d31a7b9f126ae7
Ivan <toftul.ivan@gmail.com>
2017-07-25 19:31:21 +0300

    First okay results. Added waveguide kz(w) dependence

--- main.py
+++ main.py
@@ -35,17 +35,21 @@ import const
 
 
 # ## Creating variables
-theta = 30 * np.pi / 180
+# theta = 30 * np.pi / 180
 E0_charastic = 1e4  # [V/m]
-time_charastic = 10e-3  # [s]
+#time_charastic = 10e-3  # [s]
 a_charastic = 100e-9 # [m]
 # Silicon
 density_of_particle = 2328.  # [kg / m^3]
-wave_length = 350e-9  # [nm]
-wave_length_wg = 550e-9  # [nm]
+# wave_length = 350e-9  # [nm]
+# wave_length_wg = 450e-9  # [nm]
 gamma = 0.  # phenomenological parameter
 epsilon_fiber = 4.7  # Glass
 epsilon_m = 1.  # Air
+
+
+def fd_dist(x, mu, T):
+    return(1/(1+np.exp((x-mu)/T)))
 
 
 def epsilon_particle(wl):
@@ -65,16 +69,16 @@ rho_c = 200e-9  # [m]
 # must be int
 n_mode = 1  # for HE11
 
-hight = 5-9  # [m]
+gap = 5e-9  # [m]
 
-tmax = 1e-3
-dt = tmax / 1000
+#tmax = 1e-3
+#dt = tmax / 1000
 dz = a_charastic / 100
 
 # r = {rho=x, y, z}
 dip_r = np.zeros([2, 3])
-dip_r[0] = np.array([hight, 0., 0.])
-dip_r[1] = np.array([hight, 0., 5*a_charastic])
+dip_r[0] = np.array([rho_c + gap, 0., 0.])
+dip_r[1] = np.array([rho_c + gap, 0., 5*a_charastic])
 dip_v = np.zeros([2, 3])
 dip_mu = np.zeros([2, 3], dtype=complex)
 
@@ -95,76 +99,79 @@ def G0zz(x1, y1, z1, x2, y2, z2, wl):
     kR = k1_inc(wl) * Rmod
     EXPikR4piR = np.exp(1j * kR) / (4. * np.pi * Rmod)
     A = 1 + (1j * kR - 1) / kR**2
-    B = (3 - 3j * kR - kR**2) / (kR**2 * Rmod**2) \
+    B = (3 - 3j * kR - kR**2) / (kR**2) \
                 * (z1 - z2)**2 / Rmod**2
     Gzz = EXPikR4piR * (A + B)
-        
     return(Gzz)
 
 
 # waveguide wave vector 
-def k1_wg():
-    return(2 * np.pi / wave_length_wg)
-def k2_wg():
-    return(np.sqrt(epsilon_fiber) * k1_wg())
+def k1_wg(wl):
+    return(2 * np.pi / wl)
+def k2_wg(wl):
+    return(np.sqrt(epsilon_fiber) * k1_wg(wl))
 
-def kz_wg():
-    return(k2_wg() * np.cos(theta))
-def krho1_wg():
-    return(k1_wg() * np.sin(theta))
-def krho2_wg():
-    return(k2_wg() * np.sin(theta))
+omega_c = 2*np.pi / rho_c * 2 / (np.sqrt(epsilon_fiber) + 1) * const.c
+def kz_wg(omega):
+    koef = (1 - fd_dist(omega, omega_c, omega_c*0.2)) * (np.sqrt(epsilon_fiber) - 1) + 1
+    return(koef / const.c * omega)
 
-def A_():
-    return(1/krho2_wg()**2 - 1/krho1_wg()**2)
-def B_():
-    return(sp.jvp(n_mode, krho2_wg() * rho_c) / 
-           (krho2_wg() * sp.jv(n_mode, krho2_wg() * rho_c)))
-def C_():
-    return(sp.h1vp(n_mode, krho1_wg() * rho_c) / 
-           krho1_wg() * sp.hankel1(n_mode, krho1_wg() * rho_c))
-def A_prime():
-    return(2 * kz_wg() * (1/krho2_wg()**4 - 1/krho1_wg()**4))
-def B_prime():
-    kr2 = krho2_wg()
+def krho1_wg(wl):
+    return(np.sqrt(k1_wg(wl)**2 - kz_wg(2*np.pi*const.c/wl)))
+def krho2_wg(wl):
+    return(np.sqrt(k2_wg(wl)**2 - kz_wg(2*np.pi*const.c/wl)))
+
+def A_(wl):
+    return(1/krho2_wg(wl)**2 - 1/krho1_wg(wl)**2)
+def B_(wl):
+    return(sp.jvp(n_mode, krho2_wg(wl) * rho_c) / 
+           (krho2_wg(wl) * sp.jv(n_mode, krho2_wg(wl) * rho_c)))
+def C_(wl):
+    return(sp.h1vp(n_mode, krho1_wg(wl) * rho_c) / 
+           krho1_wg(wl) * sp.hankel1(n_mode, krho1_wg(wl) * rho_c))
+def A_prime(wl):
+    return(2 * kz_wg(wl) * (1/krho2_wg(wl)**4 - 1/krho1_wg(wl)**4))
+def B_prime(wl):
+    kr2 = krho2_wg(wl)
     kr = kr2 * rho_c
     Jn = sp.jv(n_mode, kr)
     Jnp = sp.jvp(n_mode, kr)
     Jnpp = sp.jvp(n_mode, kr, 2)
     
-    B1 = - Jnpp * kz_wg() * rho_c / (kr2**2 * Jn)
-    B2 = Jnp * kz_wg() * (Jn / kr2 + rho_c * Jnp) / (kr2 * Jn)**2
+    B1 = - Jnpp * kz_wg(2*np.pi*const.c/wl) * rho_c / (kr2**2 * Jn)
+    B2 = Jnp * kz_wg(2*np.pi*const.c/wl) * (Jn / kr2 + rho_c * Jnp) / (kr2 * Jn)**2
     return(B1 + B2)
-def C_prime():
-    kr1 = krho1_wg()
+def C_prime(wl):
+    kr1 = krho1_wg(wl)
     kr = kr1 * rho_c
     Hn = sp.hankel1(n_mode, kr)
     Hnp = sp.h1vp(n_mode, kr)
     Hnpp = sp.h1vp(n_mode, kr, 2)
     
-    C1 = - Hnpp * kz_wg() * rho_c / (kr1**2 * Hn)
-    C2 = Hnp * kz_wg() * (Hn / kr1 + rho_c * Hnp) / (kr1 * Hn)**2
+    C1 = - Hnpp * kz_wg(2*np.pi*const.c/wl) * rho_c / (kr1**2 * Hn)
+    C2 = Hnp * kz_wg(2*np.pi*const.c/wl) * (Hn / kr1 + rho_c * Hnp) / (kr1 * Hn)**2
     return(C1 + C2)
-def F_():
-    A = A_()
-    Ap = A_prime()
-    B = B_()
-    Bp = B_prime()
-    C = C_()
-    Cp = C_prime()
-    kz = kz_wg()
-    k1 = k1_wg()
-    k2 = k2_wg()
+def F_(wl):
+    A = A_(wl)
+    Ap = A_prime(wl)
+    B = B_(wl)
+    Bp = B_prime(wl)
+    C = C_(wl)
+    Cp = C_prime(wl)
+    kz = kz_wg(2*np.pi*const.c/wl)
+    k1 = k1_wg(wl)
+    k2 = k2_wg(wl)
     
     F = - 2 * A * kz * epsilon_fiber * (Ap * kz + A) + \
         ((Bp - Cp) * (k2**2 * B - k1**2 * C) + (B - C) * (k2**2 * Bp - k1**2 * Cp)) * rho_c**2
     
     return(F)
-def P11NN():
-    k1 = k1_wg()
-    k2 = k2_wg()
-    kr1 = krho1_wg()
-    kr2 = krho2_wg()
+
+def P11NN(wl):
+    k1 = k1_wg(wl)
+    k2 = k2_wg(wl)
+    kr1 = krho1_wg(wl)
+    kr2 = krho2_wg(wl)
     kr1rc = kr1 * rho_c
     kr2rc = kr2 * rho_c
     Jn1 = sp.jv(n_mode, kr1rc)
@@ -173,17 +180,18 @@ def P11NN():
     Jnp2 = sp.jvp(n_mode, kr2rc)
     Hn1 = sp.hankel1(n_mode, kr1rc)
     Hnp1 = sp.h1vp(n_mode, kr1rc)
-    F = F_()
+    F = F_(wl)
     
-    P = (1/kr2**2 - 1/kr1**2) * kz_wg()**2 * epsilon_fiber - \
+    P = (1/kr2**2 - 1/kr1**2)**2 * kz_wg(2*np.pi*const.c/wl)**2 * epsilon_fiber - \
         (Jnp2 / (kr2 * Jn2) - Hnp1 / (kr1 * Hn1)) *\
         (Jnp2 * k2**2 / (Jn2 * kr2) - Jnp1 * k1**2 / (Jn1 * kr1)) * rho_c**2
     
     return(Jn1 / (F * Hn1) * P)
-def Gszz(rho, rho_prime, dz, wl = wave_length_wg):
-    G = -0.5 * P11NN() * krho1_wg()**2 / (k1_wg()**2) * \
-        sp.hankel1(0, krho1_wg()*rho) * sp.hankel1(0, krho1_wg()*rho_prime) * \
-        np.exp(1j * kz_wg() * np.abs(dz))
+
+def Gszz(rho, rho_prime, dz, wl):
+    G = -0.5 * P11NN(wl) * krho1_wg(wl)**2 / (k1_wg(wl)**2) * \
+        sp.hankel1(0, krho1_wg(wl)*rho) * sp.hankel1(0, krho1_wg(wl)*rho_prime) * \
+        np.exp(1j * kz_wg(2*np.pi*const.c/wl) * np.abs(dz))
     return(G)
 
 # mu_12 = alpha_eff * E0
@@ -205,40 +213,73 @@ def force(rho1, rho2, z, wl):
     z_plus = z + dz
     z_minus = z - dz
     
-    G_plus =  Gszz(rho1, rho2, z_plus) + \
+    G_plus =  Gszz(rho1, rho2, z_plus, wl) + \
               G0zz(rho1, 0., 0., rho2, 0., z_plus, wl)
               
-    G_minus = Gszz(rho1, rho2, z_minus) + \
+    G_minus = Gszz(rho1, rho2, z_minus, wl) + \
               G0zz(rho1, 0., 0., rho2, 0., z_minus, wl)
               
     RE = k1_inc(wl)**2 * (G_plus - G_minus) * 0.5 / dz
     
     return(Fz.real * RE.real)
 
-
-def plot_F(rho1, rho2, z, wl=wave_length):
-    f = force(rho1, rho2, z, wl)
-    plt.rcParams.update({'font.size': 13})
-    plt.figure(figsize=(7.24, 4.24))
-    plt.plot(z*1e6, f)
-    plt.title(r'a = %.0f nm, $\rho_c$ = %.0f nm, $\lambda$ = %.1f nm' % (a_charastic*1e9, rho_c*1e9, wl*1e9))
-    plt.xlabel(r'$\Delta z$, $\mu$m')
-    plt.ylabel(r'$Fz$, N')
-    plt.grid()
-    plt.show()
-    
 def plot_F_wl(rho1, rho2, z, wl):
     f = force(rho1, rho2, z, wl)
-    plt.rcParams.update({'font.size': 13})
+    plt.rcParams.update({'font.size': 12})
     plt.figure(figsize=(7.24, 4.24))
     plt.plot(wl*1e9, f)
-    plt.title(r'a = %.0f nm, $\rho_c$ = %.0f nm, $\Delta z$ = %.1f $\mu$m' % (a_charastic*1e9, rho_c*1e9, z*1e6))
+    #plt.ylim(-1e-20, 1e-20)
+    plt.title(r'a = %.0f nm, $\rho_c$ = %.0f nm, $\Delta z$ = %.1f $\mu$m' % (a_charastic*1e9, rho_c*1e9, z*1e6), loc='right')
     plt.xlabel(r'$\lambda$, nm')
     plt.ylabel(r'$Fz$, N')
     plt.grid()
     plt.show()
 
-plt.xkcd()        # on
+def plot_F(rho1, rho2, z, wl):
+    f = force(rho1, rho2, z, wl)
+    plt.rcParams.update({'font.size': 12})
+    plt.figure(figsize=(7.24, 4.24))
+    plt.plot(z*1e6, f)
+    plt.title(r'a = %.0f nm, $\rho_c$ = %.0f nm, $\lambda$ = %.1f nm' % (a_charastic*1e9, rho_c*1e9, wl*1e9), loc='right')
+    plt.xlabel(r'$\Delta z$, $\mu$m')
+    plt.ylabel(r'$Fz$, N')
+    #plt.ylim(-1e-18, 1e-18)
+    plt.grid()
+    plt.show()
+    
+def plot_alpha_z(rho1, rho2, z, wl):
+    al = alpha_eff(rho1,rho2, z, wl)
+    plt.rcParams.update({'font.size': 12})
+    plt.figure(figsize=(7.24, 4.24))
+    plt.plot(z*1e6, al)
+    plt.legend()
+    plt.title(r'a = %.0f nm, $\rho_c$ = %.0f nm, $\lambda$ = %.1f nm' % (a_charastic*1e9, rho_c*1e9, wl*1e9), loc='right')
+    plt.xlabel(r'$\Delta z$, $\mu$m')
+    plt.ylabel(r'$\alpha_{eff}$')
+    #plt.ylim(-1e-18, 1e-18)
+    plt.grid()
+    plt.show()    
+
+def plot_G_z(rho1, rho2, z, wl):
+    #al = alpha_eff(rho1,rho2, z, wl)
+    al = G0zz(rho1, 0., 0., rho2, 0., z, wl)
+    al2 = Gszz(rho1, rho2, z, wl)
+    plt.rcParams.update({'font.size': 12})
+    plt.figure(figsize=(7.24, 4.24))
+    plt.plot(z*1e6, al, label=r'$G_0$')
+    plt.plot(z*1e6, al2, label=r'$G_s$')
+    plt.legend()
+    plt.title(r'a = %.0f nm, $\rho_c$ = %.0f nm, $\lambda$ = %.1f nm' % (a_charastic*1e9, rho_c*1e9, wl*1e9), loc='right')
+    plt.xlabel(r'$\Delta z$, $\mu$m')
+    plt.ylabel('Greens function')
+    #plt.ylim(-1e-18, 1e-18)
+    plt.grid()
+    plt.show()
+
+#plt.xkcd()        # on
 #plt.rcdefaults()  # off
-plot_F_wl(hight, hight, 30*a_charastic, np.linspace(200, 1200, 400)*1e-9)
-plot_F(hight, hight, np.linspace(2*a_charastic, 100000*a_charastic, 300), 800e-9)
+plot_F_wl(rho_c + gap, rho_c + gap, 30*a_charastic, np.linspace(400, 1300, 4000)*1e-9)
+plot_F(rho_c + gap, rho_c + gap, np.linspace(0.2e-6, 1.5e-6, 30000), 600e-9)
+plot_alpha_z(rho_c + gap, rho_c + gap, np.linspace(0.2e-6, 1.5e-6, 30000), 600e-9)
+plot_G_z(rho_c + gap, rho_c + gap, np.linspace(0.2e-6, 1.5e-6, 30000), 600e-9)
+
